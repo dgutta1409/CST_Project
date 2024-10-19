a5a127b05e3151bcb20b0adb63672ce6d9185738
dgutta1409 <142830071+dgutta1409@users.noreply.github.com>
2024-10-19 14:16:29 -0500

    Update GUI_Application.py
    Initial commit

--- GUI_Application.py
+++ GUI_Application.py
@@ -1,1 +1,8 @@
+import tkinter as tk  # Import the tkinter library for GUI
+from tkinter import ttk, messagebox  # Import specific modules from tkinter
+import requests  # For making API requests to fetch data
+import pandas as pd  # For working with data in DataFrames
+import matplotlib.pyplot as plt  # For plotting graphs
+import seaborn as sns  # For advanced plotting
+from threading import Thread  # To run processes in parallel (threads)
 
